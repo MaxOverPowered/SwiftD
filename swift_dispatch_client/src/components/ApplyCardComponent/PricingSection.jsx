18033a1bd4f5e3aa49f27cc74370a6cf177bd19b
dinuie <iesanudinu01@gmail.com>
2023-04-13 16:29:03 +0300

    new PriceSection, added locomotive scrolling

--- swift_dispatch_client/src/components/ApplyCardComponent/PricingSection.jsx
+++ swift_dispatch_client/src/components/ApplyCardComponent/PricingSection.jsx
@@ -1,110 +1,116 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const plans = [
+  {
+    name: "Basic Plan",
+    price: "$9.99",
+    features: ["50 loads per month", "24/7 support"],
+    scale: [1, 1.1],
+  },
+  {
+    name: "Pro Plan",
+    price: "$19.99",
+    features: ["100 loads per month", "24/7 support", "Route optimization"],
+    scale: [1, 1.2],
+  },
+  {
+    name: "Premium Plan",
+    price: "$29.99",
+    features: [
+      "Unlimited loads per month",
+      "24/7 support",
+      "Route optimization",
+      "Dedicated account manager",
+    ],
+    scale: [1, 1.1],
+  },
+];
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    margin: "1rem",
-    boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.2)",
-    cursor: "pointer",
+  card: {
+    width: "300px",
+    height: "400px",
+    borderRadius: "20px",
+    padding: "1.5rem",
+    margin: "1.2rem",
+    boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.1)",
+    transition: "box-shadow 0.3s ease-in-out",
+    "&:hover": {
+      boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
+    },
   },
   title: {
     fontSize: "2rem",
     fontWeight: "bold",
+    fontFamily: "roboto",
     marginBottom: "1rem",
     textAlign: "center",
+    color: "#333",
   },
   price: {
     fontSize: "3rem",
     fontWeight: "bold",
+    fontFamily: "roboto",
     marginBottom: "1rem",
     textAlign: "center",
+    color: "#333",
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  pos: {
-    marginBottom: 12,
+  feature: {
+    fontSize: "1.1rem",
+    fontWeight: "semibold",
+    fontFamily: "sans-serif",
+    lineHeight: "1.8rem",
+    textAlign: "center",
+    color: "#666",
+    marginBottom: "0.5rem",
   },
 });
 
 const PricingSection = () => {
   const classes = useStyles();
+  const [ref, inView] = useInView({ threshold: 0 });
+
+  useEffect(() => {
+    console.log("inView:", inView);
+  }, [inView]);
 
   return (
     <section className="flex justify-center">
       <motion.div
         className="flex flex-wrap justify-center"
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
+        animate={inView ? { opacity: 1 } : { opacity: 0 }}
+        transition={{ duration: 1.75 }}
+        ref={ref}
       >
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          className="flex"
-        >
-          <Card className={classes.root}>
-            <CardContent>
-              <Typography className={classes.title}>Basic Plan</Typography>
-              <Typography className={classes.price}>$9.99</Typography>
-              <Typography variant="body2" component="p">
-                50 loads per month
-                <br />
-                24/7 support
-              </Typography>
-            </CardContent>
-          </Card>
-        </motion.div>
-
-        <motion.div
-          whileHover={{ scale: 1.2 }}
-          whileTap={{ scale: 0.9 }}
-          className="flex"
-        >
-          <Card className={`${classes.root} lg:w-96`}>
-            <CardContent>
-              <Typography className={classes.title}>Pro Plan</Typography>
-              <Typography className={classes.price}>$19.99</Typography>
-              <Typography variant="body2" component="p">
-                100 loads per month
-                <br />
-                24/7 support
-                <br />
-                Route optimization
-              </Typography>
-            </CardContent>
-          </Card>
-        </motion.div>
-
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          className="flex"
-        >
-          <Card className={classes.root}>
-            <CardContent>
-              <Typography className={classes.title}>Premium Plan</Typography>
-              <Typography className={classes.price}>$29.99</Typography>
-              <Typography variant="body2" component="p">
-                Unlimited loads per month
-                <br />
-                24/7 support
-                <br />
-                Route optimization
-                <br />
-                Dedicated account manager
-              </Typography>
-            </CardContent>
-          </Card>
-        </motion.div>
+        {plans.map((plan, index) => (
+          <motion.div key={index} className="flex">
+            <Card
+              className={classes.card}
+              style={{ boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.1)" }}
+            >
+              <CardContent>
+                <Typography className={classes.title} component="h2">
+                  {plan.name}
+                </Typography>
+                <Typography className={classes.price} component="h3">
+                  {plan.price}
+                </Typography>
+                {plan.features.map((feature, index) => (
+                  <Typography key={index} className={classes.feature}>
+                    {feature}
+                  </Typography>
+                ))}
+              </CardContent>
+            </Card>
+          </motion.div>
+        ))}
       </motion.div>
     </section>
   );
 };
-
 export default PricingSection;
