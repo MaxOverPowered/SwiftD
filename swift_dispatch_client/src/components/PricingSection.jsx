51396bd5c3fe785f9247c41983581b27df1e9afd
MaxOverPowered <copita.maxim.info@gmail.com>
2023-05-10 14:25:29 +0300

    Put on cards contact us button and when button is clicked contact Form is showing

--- swift_dispatch_client/src/components/PricingSection.jsx
+++ swift_dispatch_client/src/components/PricingSection.jsx
@@ -1,126 +1,13 @@
-import React, { useRef, useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardContent, Typography } from "@material-ui/core";
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-// eslint-disable-next-line no-unused-vars
-import truck_element from "../company_Icon/new/truck_element.png";
+import React from "react";
 import ServiceCard from "../pages/Services";
 
 
-const plans = [
-  {
-    name: "Car hauling dispatch service",
-    price: "from %per Gross",
-    features: ["2+years dispatch", "Keep company healthy","support in 3 languages","Route optimization","Best possible rates on the route","Good Communication with Drivers and Brokers !","appointment at pickup and delivery !"],
-    scale: [1, 1.1],
-  },
-  {
-    name: "Billing & Accounting",
-    price: " from %per Gross",
-    features: ["Documentation", "24/7 support", ],
-    scale: [1, 1.2],
-  },
-  {
-    name: "Logbook",
-    price: "from $99.99",
-    features: [
-      "Ensuring compliance with the HOS regulations is a challenging task for any company, but it's one that we've been successfully managing for years. Even back when drivers were still using paper logs, we were dedicated to ensuring full compliance. With our expert team, you can trust that your drivers will always follow the latest HOS rules and regulations",
-      "Route optimization",
-    ],
-    scale: [1, 1.1],
-  },
-];
-
-const useStyles = makeStyles({
-  card: {
-    backgroundImage: `url(${truck_element})`,
-      backgroundColor: "#FFFFF",
-    width: "600px",
-    height: "450px",
-    borderRadius: "20px",
-    padding: "1.5rem",
-    margin: "1.2rem",
-    boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.1)",
-    transition: "box-shadow 0.3s ease-in-out",
-    "&:hover": {
-      boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
-    },
-  },
-  title: {
-    fontSize: "2rem",
-    fontWeight: "bold",
-    fontFamily: "Roboto, sans-serif",
-    marginBottom: "1rem",
-    textAlign: "center",
-    textDecoration:"underline",
-    color: "#1B5E20",
-  },
-  price: {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    fontFamily:  "Roboto, sans-serif",
-    marginBottom: "1rem",
-    textAlign: "center",
-    color: "#1B5E20",
-  },
-  feature: {
-    fontSize: "1.1rem",
-    fontWeight: "semibold",
-    marginLeft: "1.5rem",
-    fontFamily: "Roboto, sans-serif",
-    lineHeight: "1.8rem",
-    textAlign: "left",
-    color: "#666",
-    marginBottom: "0.5rem",
-    listStyleType: "disc",
-    display: "list-item",
-  }
-,
-});
-
 const PricingSection = () => {
-  const classes = useStyles();
-  const [ref, inView] = useInView({ threshold: 0 });
 
-  useEffect(() => {
-    console.log("inView:", inView);
-  }, [inView]);
-
-  return (
-    <section className="flex justify-center">
-      {/*<motion.div*/}
-      {/*  className="flex flex-wrap justify-center"*/}
-      {/*  initial={{ opacity: 0 }}*/}
-      {/*  animate={inView ? { opacity: 1 } : { opacity: 0 }}*/}
-      {/*  transition={{ duration: 1.75 }}*/}
-      {/*  ref={ref}*/}
-      {/*>*/}
-      {/*  {plans.map((plan, index) => (*/}
-      {/*    <motion.div key={index} className="flex">*/}
-      {/*      <Card*/}
-      {/*        className={classes.card}*/}
-      {/*        style={{ boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.1)" }}*/}
-      {/*      >*/}
-      {/*        <CardContent>*/}
-      {/*          <Typography className={classes.title} component="h2">*/}
-      {/*            {plan.name}*/}
-      {/*          </Typography>*/}
-      {/*          <Typography className={classes.price} component="h3">*/}
-      {/*            {plan.price}*/}
-      {/*          </Typography>*/}
-      {/*          {plan.features.map((feature, index) => (*/}
-      {/*            <Typography key={index} className={classes.feature}>*/}
-      {/*              {feature}*/}
-      {/*            </Typography>*/}
-      {/*          ))}*/}
-      {/*        </CardContent>*/}
-      {/*      </Card>*/}
-      {/*    </motion.div>*/}
-      {/*  ))}*/}
-      {/*</motion.div>*/}
-      <ServiceCard/>
-    </section>
-  );
+    return (
+        <section className="flex justify-center">
+            <ServiceCard/>
+        </section>
+    );
 };
 export default PricingSection;
--- swift_dispatch_client/src/pages/Services.jsx
+++ swift_dispatch_client/src/pages/Services.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {Card} from "react-bootstrap";
 import {FaCheck} from "react-icons/fa";
 import caHaulerImage from "../company_Icon/new/1.png";
 import accountingImage from "../company_Icon/new/4.png";
 import logImage from "../company_Icon/new/3.png";
 import {motion} from "framer-motion";
+import ContactUsCard from "../components/ContactUsCard";
 
 
 const services = [
@@ -25,7 +26,7 @@ const services = [
     },
     {
         title: "Billing & Accounting",
-        price: "from " + 149+ " $",
+        price: "from " + 149 + " $",
         features: [
             "Documentation",
             "Submitting the necessary documents to the broker/factoring company to receive payment for the shipments.",
@@ -38,7 +39,7 @@ const services = [
     },
     {
         title: "Logbook",
-        price: "from " + 199+" $",
+        price: "from " + 199 + " $",
         features: [
             "Route optimization",
             "We've successfully managed to ensure compliance with HOS regulations for years, even before drivers used electronic logs. Trust our expert team to ensure your drivers always follow the latest HOS rules.",
@@ -49,10 +50,14 @@ const services = [
     },
 ];
 
-const ServiceCard = ({service}) => {
+const ServiceCard = ({service, onClick}) => {
     return (
         <Card className="flex flex-col justify-between h-full"
-              style={{backgroundImage: `url(${service.backgroundImage})`, backgroundSize: service.backgroundSize, backgroundPosition: 'center'}}>
+              style={{
+                  backgroundImage: `url(${service.backgroundImage})`,
+                  backgroundSize: service.backgroundSize,
+                  backgroundPosition: 'center'
+              }}>
             <div>
                 <div className="bg-green-800 text-white py-2 px-4 rounded-t-lg">
                     <h3 className="text-lg font-bold h-12 flex items-center justify-between">
@@ -63,45 +68,62 @@ const ServiceCard = ({service}) => {
                 <ul className="text-gray-700 p-4 flex-1">
                     {service.features.map((feature, index) => (
                         <li key={index} className="flex items-start mb-2">
-                            <FaCheck className="mr-2 mt-1 text-green-500 text-xl flex-shrink-0" />
+                            <FaCheck className="mr-2 mt-1 text-green-500 text-xl flex-shrink-0"/>
                             <span className="align-self-start">{feature}</span>
                         </li>
                     ))}
                 </ul>
             </div>
             <div className="bg-green-800 py-4 px-4 rounded-b-lg">
-                <button >
-                    {/*<FiShoppingCart className="mr-2"/>*/}
-                    {/*Add to Cart*/}
-                    <motion.button
-                        className="bg-white py-3 px-6 rounded-full text-lg font-bold text-gray-900 hover:bg-gray-300 hover:text-gray-900 transition-opacity duration-300 ease-in-out opacity-80"
-                        // onClick={handleContactClick}
-                        whileHover={{ scale: 1.2 }}
-                        transition={{ duration: 0.3 }}
-                        style={{ opacity: 0.8 }}
-                        data-scroll
-                    >
-                        Contact Us
-                    </motion.button>
-                </button>
+                <motion.button
+                    className="bg-white py-3 px-6 rounded-full text-lg font-bold text-gray-900 hover:bg-gray-300 hover:text-gray-900 transition-opacity duration-300 ease-in-out opacity-80"
+                    onClick={() => onClick(service)}
+                    whileHover={{scale: 1.2}}
+                    transition={{duration: 0.3}}
+                    style={{opacity: 0.8}}
+                    data-scroll
+                >
+                    Contact Us
+                </motion.button>
             </div>
         </Card>
     );
 };
 
+
 const ServicePage = () => {
+    const [showContactForm, setShowContactForm] = useState(false);
+
+    const handleContactClick = () => {
+        setShowContactForm(true);
+    };
+
+    const handleFormClose = () => {
+        setShowContactForm(false);
+    };
+
     return (
         <div className="container mx-auto py-8">
-            <h1 className="text-4xl font-bold mb-8">Our Services</h1>
             <div className="flex flex-wrap -mx-4">
-                {services.map((service, index) => (
-                    <div className="w-full sm:w-1/2 lg:w-1/3 p-4" key={index}>
-                        <ServiceCard service={service}/>
+                {showContactForm ? (
+                    <div className="">
+                        <div className="contact-form-container slide-in flex h-screen item-centre justify-center opacity-75 bg-black">
+                            <ContactUsCard onClose={handleFormClose} />
+                        </div>
                     </div>
-                ))}
+
+                ) : (
+                    services.map((service, index) => (
+                        <div className="w-full sm:w-1/2 lg:w-1/3 p-4" key={index}>
+                            <ServiceCard service={service} onClick={handleContactClick} />
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     );
 };
+
+
 
 export default ServicePage;
