d3fb7e31be2404fb16c054576f411bfaa9c7bbb1
MaxOverPowered <copita.maxim.info@gmail.com>
2023-05-11 13:21:43 +0300

    Create loading and add it when app is open

--- swift_dispatch_client/src/components/Loading.jsx
+++ swift_dispatch_client/src/components/Loading.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import LoadingStyle from '../style/LoadingStyle';
-import SwiftIcon from '../company_Icon/logo_transparent_3.png';
+import logo from "../company_Icon/new/text_element.png";
 
 const Loading = () => (
-    <div className="App">
-        <LoadingStyle />
-        <header className="App-header">
-            <img src={SwiftIcon} className="App-logo" alt="logo" />
-        </header>
+    <div className="flex justify-center items-center h-screen">
+        <div className="flex items-center item-left sm:h-15 ml-12 -mt-5">
+            <a>
+                <img
+                    src={logo}
+                    alt="swift_Dispatch"
+                    className="object-contain max-w-full h-auto -ml-35 animate-pulse"
+                    style={{maxWidth: "200px", maxHeight: "200px"}}
+                />
+            </a>
+        </div>
     </div>
 );
 
