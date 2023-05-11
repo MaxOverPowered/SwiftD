7c70a79fe9e01ec40fd87bc9021ceec36d306143
dinuie <iesanudinu01@gmail.com>
2023-05-11 13:22:28 +0300

    redirect to contactus

--- swift_dispatch_client/src/components/ContactUsCard.jsx
+++ swift_dispatch_client/src/components/ContactUsCard.jsx
@@ -1,179 +1,178 @@
-import React, {useEffect, useState} from "react";
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
-import {sendEmail} from "../utils/ApiUtils";
-import {notification} from "antd";
-
+import React, { useEffect, useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import { sendEmail } from "../utils/ApiUtils";
+import { notification } from "antd";
 
 const ContactCard = (props) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [contact, setContact] = useState("");
-    const [message, setMessage] = useState("");
-    const [charCount, setCharCount] = useState(500);
-    const [submitDisable, setSubmitDisable] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
+  const [message, setMessage] = useState("");
+  const [charCount, setCharCount] = useState(500);
+  const [submitDisable, setSubmitDisable] = useState(true);
 
-    const handleSendClick = () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
-    function handleMessageChange(event) {
-        const newInputText = event.target.value;
-        const remainingChars = 500 - newInputText.length;
-        if (remainingChars >= 0) {
-            setCharCount(remainingChars);
-            setMessage(newInputText);
-        }
+  const handleSendClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  function handleMessageChange(event) {
+    const newInputText = event.target.value;
+    const remainingChars = 500 - newInputText.length;
+    if (remainingChars >= 0) {
+      setCharCount(remainingChars);
+      setMessage(newInputText);
     }
+  }
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const handleContactChange = (value) => {
-        setContact(value);
-    };
+  const handleContactChange = (value) => {
+    setContact(value);
+  };
 
-    useEffect(() => {
-        if (name === "" || email === "" || contact === "" || message === "") {
-            setSubmitDisable(true)
-        } else {
-            setSubmitDisable(false)
-            // props.onClose();
-        }
-
-    }, [name, email, contact, message])
-    const handleClose = () => {
-        props.onClose();
-    };
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        if (!isValidEmail(email)) {
-            notification.error({description: 'Invalid Email'}
-            );
-        } else {
-            const templateParams = {
-                clientName: name,
-                clientMessage: message,
-                clientContactNumber: contact,
-                clientEmail: email
-            };
-            sendEmail(templateParams).then(response => {
-                setName("");
-                setEmail("");
-                setContact("");
-                setMessage("");
-            })
-        }
-    };
-
-    const isValidEmail = (email) => {
-        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return emailRegex.test(String(email).toLowerCase());
+  useEffect(() => {
+    if (name === "" || email === "" || contact === "" || message === "") {
+      setSubmitDisable(true);
+    } else {
+      setSubmitDisable(false);
+      // props.onClose();
     }
+  }, [name, email, contact, message]);
+  const handleClose = () => {
+    props.onClose();
+  };
 
-    return (
-        <div className="p-8 md:p-10 lg:p-12 bg-white rounded-lg shadow-md">
-            <div className="relative h-32 w-full">
-                <div className="absolute left-0 top-0 h-16 w-16">
-                    <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-green-900 mb-6">
-                        Contact Us
-                    </h2></div>
-                <div className="absolute top-0 right-0 h-16 w-16">
-                    <button
-                        className="absolute top-0 right-0 p-2 focus:outline-none"
-                        onClick={handleClose}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6 text-gray-500 hover:text-gray-700"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-            <form className="grid grid-cols-1 md:grid-cols-2 gap-6" onSubmit={handleSubmit}>
-                <div className="flex flex-col">
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        onChange={handleNameChange}
-                        className="border-gray-300 bg-gray-200 w-full px-4 py-2 mb-2"
-                        placeholder="Name"
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <input
-                        id="email"
-                        name="email"
-                        onChange={handleEmailChange}
-                        className="border-gray-300 bg-gray-200 w-full px-4 py-2 mb-2"
-                        placeholder="Email"
-                    />
-                </div>
-                <div className="flex flex-col ">
-                    <div className="relative ">
-                        <PhoneInput className="border-gray-300 w-full px-10"
-                                    country={'us'}
-                                    value={contact}
-                                    onChange={handleContactChange}
-                                    inputProps={{
-                                        name: 'contact',
-                                        required: true,
-                                        placeholder: 'Enter your phone number',
-                                        className: ' border-gray-300 bg-gray-100  w-full px-11 py-2',
-                                    }}
-                        /></div>
-                </div>
-                <div className="flex flex-col">
-                    <label
-                        htmlFor="message"
-                        className="text-green-900 font-medium mb-2"
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-                    >
-                        Message
-                    </label>
-                    <textarea
-                        className="border-gray-300 bg-gray-200 w-full px-4 py-2 rounded-md h-full"
-                        maxLength={500}
-                        rows={11}
-                        cols={50}
-                        value={message}
-                        onChange={handleMessageChange}
-                        placeholder="Please leave some details about your company here, for example, how many drivers you have, whether you're interested in collaborating with us, or if you'd like to learn about our benefits."
+    if (!isValidEmail(email)) {
+      notification.error({ description: "Invalid Email" });
+    } else {
+      const templateParams = {
+        clientName: name,
+        clientMessage: message,
+        clientContactNumber: contact,
+        clientEmail: email,
+      };
+      sendEmail(templateParams).then((response) => {
+        setName("");
+        setEmail("");
+        setContact("");
+        setMessage("");
+      });
+    }
+  };
 
-                    />
-                    <p className="text-right text-sm text-gray-500">
-                        {message.length}/500 characters
-                    </p>
-                </div>
-                <div className="flex flex-col md:col-span-2">
-                    <button onClick={handleSendClick}
-                        disabled={submitDisable}
-                        type="submit"
-                        className="text-white bg-green-500 hover:bg-green-600 px-4 py-2 rounded-md"
-                    >
-                        Send
-                    </button>
-                </div>
-            </form>
+  const isValidEmail = (email) => {
+    const emailRegex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(String(email).toLowerCase());
+  };
+
+  return (
+    <div className="p-8 md:p-10 lg:p-12 bg-white rounded-lg shadow-md">
+      <div className="relative h-32 w-full">
+        <div className="absolute left-0 top-0 h-16 w-16">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-green-900 mb-6">
+            Contact Us
+          </h2>
         </div>
-    );
-}
+        <div className="absolute top-0 right-0 h-16 w-16">
+          <button
+            className="absolute top-0 right-0 p-2 focus:outline-none"
+            onClick={handleClose}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-gray-500 hover:text-gray-700"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <form
+        className="grid grid-cols-1 md:grid-cols-2 gap-6"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleNameChange}
+            className="border-gray-300 bg-gray-200 w-full px-4 py-2 mb-2"
+            placeholder="Name"
+          />
+        </div>
+        <div className="flex flex-col">
+          <input
+            id="email"
+            name="email"
+            onChange={handleEmailChange}
+            className="border-gray-300 bg-gray-200 w-full px-4 py-2 mb-2"
+            placeholder="Email"
+          />
+        </div>
+        <div className="flex flex-col ">
+          <div className="relative ">
+            <PhoneInput
+              className="border-gray-300 w-full px-10"
+              country={"us"}
+              value={contact}
+              onChange={handleContactChange}
+              inputProps={{
+                name: "contact",
+                required: true,
+                placeholder: "Enter your phone number",
+                className: " border-gray-300 bg-gray-100  w-full px-11 py-2",
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="message" className="text-green-900 font-medium mb-2">
+            Message
+          </label>
+          <textarea
+            className="border-gray-300 bg-gray-200 w-full px-4 py-2 rounded-md h-full resize-none"
+            maxLength={500}
+            rows={11}
+            cols={50}
+            value={message}
+            onChange={handleMessageChange}
+            placeholder="Please leave some details about your company here, for example, how many drivers you have, whether you're interested in collaborating with us, or if you'd like to learn about our benefits."
+          />
+          <p className="text-right text-sm text-gray-500">
+            {message.length}/500 characters
+          </p>
+        </div>
+        <div className="flex flex-col md:col-span-2">
+          <button
+            onClick={handleSendClick}
+            disabled={submitDisable}
+            type="submit"
+            className="text-white bg-green-500 hover:bg-green-600 px-4 py-2 rounded-md"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 export default ContactCard;
--- swift_dispatch_client/src/pages/Services.jsx
+++ swift_dispatch_client/src/pages/Services.jsx
@@ -1,129 +1,113 @@
-import React, {useState} from "react";
-import {Card} from "react-bootstrap";
-import {FaCheck} from "react-icons/fa";
+import React from "react";
+import { Card } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
 import caHaulerImage from "../company_Icon/new/1.png";
 import accountingImage from "../company_Icon/new/4.png";
 import logImage from "../company_Icon/new/3.png";
-import {motion} from "framer-motion";
-import ContactUsCard from "../components/ContactUsCard";
-
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 const services = [
-    {
-        title: "Car hauling dispatch service",
-        price: " %",
-        features: [
-            "First load on us 😊",
-            "2+ years dispatch🎧",
-            "Best possible rates on the route  💰",
-            "Good Communication 📞",
-            "Keep company healthy🌟",
-            "Appointment at pickup and delivery 📅",
-        ],
-        backgroundImage: caHaulerImage,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-    },
-    {
-        title: "Billing & Accounting",
-        price: "from " + 149 + " $",
-        features: [
-            "Documentation",
-            "Submitting the necessary documents to the broker/factoring company to receive payment for the shipments.",
-            "Resolving issues that may result from delayed payment reception.",
-            "Tracking the receipt of payments",
-        ],
-        backgroundImage: accountingImage,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-    },
-    {
-        title: "Logbook",
-        price: "from " + 199 + " $",
-        features: [
-            "Route optimization",
-            "We've successfully managed to ensure compliance with HOS regulations for years, even before drivers used electronic logs. Trust our expert team to ensure your drivers always follow the latest HOS rules.",
-        ],
-        backgroundImage: logImage,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-    },
+  {
+    title: "Car hauling dispatch service",
+    price: " %",
+    features: [
+      "First load on us 😊",
+      "2+ years dispatch🎧",
+      "Best possible rates on the route  💰",
+      "Good Communication 📞",
+      "Keep company healthy🌟",
+      "Appointment at pickup and delivery 📅",
+    ],
+    backgroundImage: caHaulerImage,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  },
+  {
+    title: "Billing & Accounting",
+    price: "from " + 149 + " $",
+    features: [
+      "Documentation",
+      "Submitting the necessary documents to the broker/factoring company to receive payment for the shipments.",
+      "Resolving issues that may result from delayed payment reception.",
+      "Tracking the receipt of payments",
+    ],
+    backgroundImage: accountingImage,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  },
+  {
+    title: "Logbook",
+    price: "from " + 199 + " $",
+    features: [
+      "Route optimization",
+      "We've successfully managed to ensure compliance with HOS regulations for years, even before drivers used electronic logs. Trust our expert team to ensure your drivers always follow the latest HOS rules.",
+    ],
+    backgroundImage: logImage,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  },
 ];
 
-const ServiceCard = ({service, onClick}) => {
-    return (
-        <Card className="flex flex-col justify-between h-full"
-              style={{
-                  backgroundImage: `url(${service.backgroundImage})`,
-                  backgroundSize: service.backgroundSize,
-                  backgroundPosition: 'center'
-              }}>
-            <div>
-                <div className="bg-green-800 text-white py-2 px-4 rounded-t-lg">
-                    <h3 className="text-lg font-bold h-12 flex items-center justify-between">
-                        <span>{service.title}</span>
-                        <span className="text-sm">{service.price}</span>
-                    </h3>
-                </div>
-                <ul className="text-gray-700 p-4 flex-1">
-                    {service.features.map((feature, index) => (
-                        <li key={index} className="flex items-start mb-2">
-                            <FaCheck className="mr-2 mt-1 text-green-500 text-xl flex-shrink-0"/>
-                            <span className="align-self-start">{feature}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="bg-green-800 py-4 px-4 rounded-b-lg">
-                <motion.button
-                    className="bg-white py-3 px-6 rounded-full text-lg font-bold text-gray-900 hover:bg-gray-300 hover:text-gray-900 transition-opacity duration-300 ease-in-out opacity-80"
-                    onClick={() => onClick(service)}
-                    whileHover={{scale: 1.2}}
-                    transition={{duration: 0.3}}
-                    style={{opacity: 0.8}}
-                    data-scroll
-                >
-                    Contact Us
-                </motion.button>
-            </div>
-        </Card>
-    );
-};
+const ServiceCard = ({ service, onClick }) => {
+  const navigate = useNavigate();
 
+  const handleContactClick = () => {
+    onClick();
+    navigate("/contact");
+  };
+
+  return (
+    <Card className="flex flex-col justify-between h-full">
+      <div>
+        <div className="bg-green-800 text-white py-2 px-4 rounded-t-lg">
+          <h3 className="text-lg font-bold h-12 flex items-center justify-between">
+            <span>{service.title}</span>
+            <span className="text-sm">{service.price}</span>
+          </h3>
+        </div>
+        <ul className="text-gray-700 p-4 flex-1">
+          {service.features.map((feature, index) => (
+            <li key={index} className="flex items-start mb-2">
+              <FaCheck className="mr-2 mt-1 text-green-500 text-xl flex-shrink-0" />
+              <span className="align-self-start">{feature}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="bg-green-800 py-4 px-4 rounded-b-lg">
+        <motion.button
+          className="bg-white py-3 px-6 rounded-full text-lg font-bold text-gray-900 hover:bg-gray-300 hover:text-gray-900 transition-opacity duration-300 ease-in-out opacity-80"
+          onClick={handleContactClick}
+          whileHover={{ scale: 1.2 }}
+          transition={{ duration: 0.3 }}
+        >
+          Contact Us
+        </motion.button>
+      </div>
+    </Card>
+  );
+};
 
 const ServicePage = () => {
-    const [showContactForm, setShowContactForm] = useState(false);
+  const handleContactClick = () => {
+    // Handle contact button click
+  };
 
-    const handleContactClick = () => {
-        setShowContactForm(true);
-    };
-
-    const handleFormClose = () => {
-        setShowContactForm(false);
-    };
-
-    return (
-        <div className="container mx-auto py-8">
-            <div className="flex flex-wrap -mx-4">
-                {showContactForm ? (
-                    <div className="">
-                        <div className="contact-form-container slide-in flex h-screen item-centre justify-center opacity-75 bg-black">
-                            <ContactUsCard onClose={handleFormClose} />
-                        </div>
-                    </div>
-
-                ) : (
-                    services.map((service, index) => (
-                        <div className="w-full sm:w-1/2 lg:w-1/3 p-4" key={index}>
-                            <ServiceCard service={service} onClick={handleContactClick} />
-                        </div>
-                    ))
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {services.map((service, index) => (
+          <ServiceCard
+            key={index}
+            service={service}
+            onClick={handleContactClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
-
-
 
 export default ServicePage;
