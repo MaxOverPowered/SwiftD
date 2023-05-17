f1c3be7f64047d92aec8b54c7046ef9435018e53
dinuie <iesanudinu01@gmail.com>
2023-05-17 15:27:56 +0300

    aboutus fixed

--- swift_dispatch_client/src/components/InfoSection.jsx
+++ swift_dispatch_client/src/components/InfoSection.jsx
@@ -1,78 +1,95 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import truckIcon from '../company_Icon/new/truck_element.png';
-import './language/resurse';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import truckIcon from "../company_Icon/new/truck_element.png";
+import "./language/resurse";
 
 const Benefits = () => {
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-    const benefits = [
-        {
-            title: t('benefits.title1'),
-            description: t('benefits.description1')
-        },
-        {
-            title: t('benefits.title2'),
-            description: t('benefits.description2')
-        },
-        {
-            title: t('benefits.title3'),
-            description: t('benefits.description3')
-        },
-        {
-            title: t('benefits.title4'),
-            description: t('benefits.description4')
-        }
-    ];
+  const benefits = [
+    {
+      title: t("benefits.title1"),
+      description: t("benefits.description1"),
+    },
+    {
+      title: t("benefits.title2"),
+      description: t("benefits.description2"),
+    },
+    {
+      title: t("benefits.title3"),
+      description: t("benefits.description3"),
+    },
+    {
+      title: t("benefits.title4"),
+      description: t("benefits.description4"),
+    },
+  ];
 
-    const switchLanguage = () => {
-        const lang = i18n.language === 'en' ? 'ru' : 'en';
-        i18n.changeLanguage(lang);
-    }
+  const switchLanguage = () => {
+    const lang = i18n.language === "en" ? "ru" : "en";
+    i18n.changeLanguage(lang);
+  };
 
-    return (
-        <div className="flex flex-col lg:flex-row items-center py-16">
-            <div className="lg:w-1/3">
-                <img src={truckIcon} alt="Truck" className="w-full max-w-sm mx-auto lg:mx-0" />
-            </div>
-            <div className="lg:w-2/3 lg:pl-16">
-                <h2 className="text-l font-bold text-gray-900 mb-4">
-                    <div key={t('infoSection.title')}>
-                        <h2 className="text-3xl font-bold text-gray-900 mb-2">{t('infoSection.title')}</h2>
-                        <p className="text-gray-700 leading-relaxed mb-6">{t('infoSection.description')}</p>
-                    </div>
-                </h2>
-                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                    {benefits.map(benefit => (
-                        <BenefitCard key={benefit.title} title={benefit.title} description={benefit.description} />
-                    ))}
-                </ul>
-                <button onClick={switchLanguage} className="mt-8 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-lg">
-                    {i18n.language === 'en' ? 'Switch to Russian' : 'Switch to English'}
-                </button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col lg:flex-row items-center py-16">
+      <div className="lg:w-1/3">
+        <img
+          src={truckIcon}
+          alt="Truck"
+          className="w-full max-w-sm mx-auto lg:mx-0"
+        />
+      </div>
+      <div className="lg:w-2/3 lg:pl-16">
+        <h2 className="text-l font-bold text-gray-900 mb-4">
+          <div key={t("infoSection.title")}>
+            <h2 className="text-3xl font-bold text-gray-900 mb-2">
+              {t("infoSection.title")}
+            </h2>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              {t("infoSection.description")}
+            </p>
+          </div>
+        </h2>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {benefits.map((benefit) => (
+            <BenefitCard
+              key={benefit.title}
+              title={benefit.title}
+              description={benefit.description}
+            />
+          ))}
+        </ul>
+        <button
+          onClick={switchLanguage}
+          className="mt-8 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-lg"
+        >
+          {i18n.language === "en" ? "Switch to Russian" : "Switch to English"}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const BenefitCard = ({ title, description }) => {
-    return (
-        <li className="relative bg-white rounded-lg p-6 text-center hover:shadow-lg transition duration-300">
-            <h3 className="text-gray-700 font-bold text-lg lg:text-xl mb-2">{title}</h3>
-            <p className="text-gray-700 leading-relaxed">{description}</p>
-            <div className="absolute bottom-0 left-0 w-full h-1 bg-green-500"></div>
-        </li>
-    );
+  return (
+    <li className="relative bg-white rounded-lg p-6 text-center hover:shadow-lg transition duration-300">
+      <h3 className="text-gray-700 font-bold text-lg lg:text-xl mb-2">
+        {title}
+      </h3>
+      <p className="text-gray-700 leading-relaxed">{description}</p>
+      <div className="absolute bottom-0 left-0 w-full h-1 bg-green-500"></div>
+    </li>
+  );
 };
 
 const InfoSection = () => {
-    return (
-        <section className="bg-gray-100 py-10">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <Benefits />
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="bg-gray-100 py-10">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <Benefits />
+      </div>
+    </section>
+  );
+};
 
 export default InfoSection;
--- swift_dispatch_client/src/pages/AboutUs.jsx
+++ swift_dispatch_client/src/pages/AboutUs.jsx
@@ -6,49 +6,56 @@ import Logo from "../company_Icon/new/logo_swift_FINAL.png";
 import ReactPlayer from "react-player";
 
 const AboutUs = () => {
-    return (
-        <div className="py-10">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex flex-wrap justify-center items-center">
-                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 md:mb-0">
-                        <img src={Logo} alt="Company Logo" className="w-full max-w-xs mx-auto"/>
-                    </div>
-                    <div className="w-full sm:w-1/2 md:w-2/3 lg:w-3/4 md:pl-12">
-                        <h2 className="text-3xl font-bold text-gray-900 mb-2">About Our Company</h2>
-                        <p className="text-gray-700 leading-relaxed mb-6">
-                            At Swift Dispatch, we're more than just a logistics company. We're
-                            a team of passionate car-hauling experts who love making our clients
-                            smile. With over 2 years of experience in the industry, we know
-                            America like the back of our hand, and we pride ourselves on our
-                            ability to efficiently and effectively dispatch our drivers to
-                            their destinations. We like our work because we love money, but we
-                            love our clients even more.
-                        </p>
-                        <div className="flex items-center mb-4">
-                            <FaMapMarkerAlt className="text-gray-600 mr-2"/>
-                            <p className="text-gray-700">Serving all of America</p>
-                        </div>
-                        <div className="flex items-center mb-4">
-                            <FiTruck className="text-gray-600 mr-2"/>
-                            <p className="text-gray-700">  Experienced drivers and top-of-the-line equipment to make your
-                                car-hauling experience a breeze 🚚</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="py-10">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap justify-center items-center">
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 md:mb-0">
+            <img
+              src={Logo}
+              alt="Company Logo"
+              className="w-full max-w-xs mx-auto"
+            />
+          </div>
+          <div className="w-full sm:w-1/2 md:w-2/3 lg:w-3/4 md:pl-12">
+            <h2 className="text-3xl font-bold text-gray-900 mb-2">
+              About Our Company
+            </h2>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              At Swift Dispatch, we're more than just a logistics company. We're
+              a team of passionate car-hauling experts who love making our
+              clients smile. With over 2 years of experience in the industry, we
+              know America like the back of our hand, and we pride ourselves on
+              our ability to efficiently and effectively dispatch our drivers to
+              their destinations. We like our work because we love money, but we
+              love our clients even more.
+            </p>
+            <div className="flex items-center mb-4">
+              <FaMapMarkerAlt className="text-gray-600 mr-2" />
+              <p className="text-gray-700">Serving all of America</p>
             </div>
-            <div className="w-full flex justify-center items-center">
-                <div className="relative mx-auto">
-                    <ReactPlayer
-                        url="https://www.youtube.com/watch?v=d1GlSl0pcF0"
-                        className="absolute top-0 left-0 w-full h-full"
-                        controls={true}
-                    />
-                </div>
+            <div className="flex items-center mb-4">
+              <FiTruck className="text-gray-600 mr-2" />
+              <p className="text-gray-700">
+                Experienced drivers and top-of-the-line equipment to make your
+                car-hauling experience a breeze 🚚
+              </p>
             </div>
-
-
+          </div>
         </div>
-        );
+      </div>
+      <div className="w-full flex justify-center items-center mt-8">
+        <div className="max-w-6xl">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=d1GlSl0pcF0"
+            controls={true}
+            width="100%"
+            height="auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
